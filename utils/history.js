00b8d11e8585cc15dc2b52ff72544e5276273dd8
ndajiya <ndajiyas@gmail.com>
2025-07-11 14:30:22 +0000

    Update history.js

--- utils/history.js
+++ utils/history.js
@@ -1,1 +1,16 @@
+// utils/history.js
 
+export async function getHistory() {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.history.search(
+        { text: '', startTime: 0, maxResults: 1000 },
+        function (results) {
+          resolve(results);
+        }
+      );
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
